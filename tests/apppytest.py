4a589cc3f60c787753270215ef932fa030ae5d83
Manjesh Setty <manjesh_n@hotmail.com>
2021-04-10 22:55:31 +0530

    commit

---
+++ tests/apppytest.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv, find_dotenv
+import os
+import pytest
+
+
+def test_fun():
+    load_dotenv(find_dotenv())
+    cname = os.getenv("cname")
+    apikey = os.getenv("apikey")
+    print(cname)
